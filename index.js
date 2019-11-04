52361a037ad72a7347e06b28293ce33726e61219
Helga Run Steinarsdottir <helgarunst@gmail.com>
2019-11-04 22:00:23 +0000

    ..

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-
 // Creates the endpoint for our webhook 
 app.post('/webhooks/facebook/webhook', (req, res) => {  
  
